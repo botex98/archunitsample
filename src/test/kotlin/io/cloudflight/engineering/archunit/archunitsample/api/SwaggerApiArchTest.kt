fbd0724dfa842fd75b38a3dd735a0dd9a2367cf6
Botond Lovasz <Botond.Lovasz@cloudflight.io>
2022-07-12 11:04:48 +0300

    - Rework SwaggerApiArchTest

--- src/test/kotlin/io/cloudflight/engineering/archunit/archunitsample/api/SwaggerApiArchTest.kt
+++ src/test/kotlin/io/cloudflight/engineering/archunit/archunitsample/api/SwaggerApiArchTest.kt
@@ -14,45 +14,46 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestPart
+import java.util.*
 
 @AnalyzeClasses(
-    packages = ["io.cloudflight.engineering.archunit"],
-    importOptions = [ImportOption.DoNotIncludeTests::class]
+    packages = ["io.cloudflight.engineering.archunit"], importOptions = [ImportOption.DoNotIncludeTests::class]
 )
 class SwaggerApiArchTest {
 
     @ArchTest
-    val api_classes_should_be_annotated_with_swagger: ArchRule = classes()
-        .that().resideInAPackage("..api")
-        .should().beAnnotatedWith("io.swagger.annotations.Api")
-        .`as`("we want the swagger generator to generate frontend DTOs and services for us")
+    val api_classes_should_be_annotated_with_swagger: ArchRule =
+        classes().that().resideInAPackage("..api")
+            .should().beAnnotatedWith("io.swagger.annotations.Api")
+            .`as`("we want the swagger generator to generate frontend DTOs and services for us")
 
     @ArchTest
-    val post_or_put_methods_should_define_consumes_argument: ArchRule = methods()
-        .that().areAnnotatedWith("org.springframework.web.bind.annotation.PostMapping")
-        .or().areAnnotatedWith("org.springframework.web.bind.annotation.PutMapping")
-        .should(defineConsumesArgument())
-        .`as`("request header Content-Type must be set so that generated frontend services work as expected")
+    val post_or_put_methods_should_define_consumes_argument: ArchRule =
+        methods().that().areAnnotatedWith("org.springframework.web.bind.annotation.PostMapping").or()
+            .areAnnotatedWith("org.springframework.web.bind.annotation.PutMapping")
+            .should(defineConsumesArgument())
+            .`as`("request header Content-Type must be set so that generated frontend services work as expected")
 
     private fun defineConsumesArgument(): ArchCondition<in JavaMethod> {
         return object : ArchCondition<JavaMethod>("define the 'consumes' argument") {
             override fun check(method: JavaMethod, events: ConditionEvents) {
-                val requestPartOrBody = listOf(RequestPart::class.java, RequestBody::class.java)
-                val containsRequestPartOrBody = method.reflect().parameterAnnotations
-                    .flatMap { it.map { kclass -> kclass.annotationClass.java } }
-                    .any { requestPartOrBody.contains(it) }
-                try {
-                    val postMappingAnnotation = method.getAnnotationOfType(PostMapping::class.java)
-                    if (containsRequestPartOrBody && postMappingAnnotation.consumes.isEmpty()) {
-                        val message: String = getMessage(method, PostMapping::class.java.simpleName)
-                        events.add(SimpleConditionEvent(method, false, message))
+                val postMapping = method.tryGetAnnotationOfType(PostMapping::class.java)
+                val putMapping = method.tryGetAnnotationOfType(PutMapping::class.java)
+
+                val hasParameterAnnotationPartOrBody = method.parameterAnnotations[0]
+                    .map { it.type.name }
+                    .any {
+                        it == RequestBody::class.java.canonicalName || it == RequestPart::class.java.canonicalName
                     }
-                } catch (ignored: Exception) {
-                    val putMappingAnnotation = method.getAnnotationOfType(PutMapping::class.java)
-                    if (containsRequestPartOrBody && putMappingAnnotation.consumes.isEmpty()) {
-                        val message: String = getMessage(method, PutMapping::class.java.simpleName)
-                        events.add(SimpleConditionEvent(method, false, message))
-                    }
+
+                if (hasParameterAnnotationPartOrBody && postMapping.isPresent && postMapping.get().consumes.isEmpty()) {
+                    val message: String = getMessage(method, PostMapping::class.java.simpleName)
+                    events.add(SimpleConditionEvent(method, false, message))
+                }
+
+                if (hasParameterAnnotationPartOrBody && putMapping.isPresent && putMapping.get().consumes.isEmpty()) {
+                    val message: String = getMessage(method, PostMapping::class.java.simpleName)
+                    events.add(SimpleConditionEvent(method, false, message))
                 }
             }
 
